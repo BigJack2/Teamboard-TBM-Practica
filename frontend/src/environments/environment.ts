4ca4a94b0542c31e6fc863002a03342f573fae3f
BigJack2 <jsvanegas2014@gmail.com>
2022-03-01 09:11:15 -0500

    Proyecto antes de agregar angular material, recien se creo la carpeta services

---
+++ frontend/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  //aca se enlaza nodejs es decir el backend
+  //este es el directorio raiz de node
+  APP_URL: 'http://localhost:3001/api/',
+};
